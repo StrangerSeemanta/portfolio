1c476366d77cc8b41ef5d2ae54ebe71fb0adb0a9
StrangerSeemanta <ssworkmail22@gmail.com>
2026-03-17 19:02:23 +0600

    Seperated two interface, one for dynamic ,another for static social heads

--- lib/db/socialdata.ts
+++ lib/db/socialdata.ts
@@ -1,15 +1,19 @@
 import { ObjectId } from "mongodb";
 import { getCollection } from "./db";
+
 export interface SocialData {
-  _id: ObjectId;
   label: string;
   href: string;
 }
+export interface SocialDataWithObjectId extends SocialData {
+  _id: ObjectId;
+}
+
 export async function getSocialMediaData() {
   try {
     const social_data_collection = await getCollection("portfolio", "socials");
     const social_data_list = await social_data_collection.find({}).toArray();
-    const response = social_data_list as unknown as SocialData[];
+    const response = social_data_list as unknown as SocialDataWithObjectId[];
     return response;
   } catch (error) {
     throw new Error(String(error));
